3bad871374c123b254681fd6c4749bd971aac58f
andikayopr <ayprihartomo@gmail.com>
2018-11-03 11:11:46 +0700

    Add public profile (all, handle, user) routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -19,6 +19,7 @@ router.get(
     (req, res) => {
         const errors = {};
         Profile.findOne({ user: req.user.id })
+            .populate('user', ['name', 'avatar'])
             .then(profile => {
                 if (!profile) {
                     errors.no_profile = 'There is no profile for this user';
@@ -29,6 +30,64 @@ router.get(
             .catch(err => res.status(404).json(err));
     }
 );
+
+// @router 	GET api/profile/handle:handle
+// @desc 	Get profile by handle
+// @access	Public
+
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+    Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.no_profile = "There's no profile for this user";
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
+
+// @router 	GET api/profile/user:user_id
+// @desc 	Get profile by user ID
+// @access	Public
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.params.user_id })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.no_profile = "There's no profile for this user";
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err =>
+            res
+                .status(404)
+                .json({ profile: 'There is no profile for this user' })
+        );
+});
+
+// @router 	GET api/profile/all
+// @desc 	Get all profiles
+// @access	Public
+router.get('/all', (req, res) => {
+    const errors = {};
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.no_profile = 'There are no profiles';
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err =>
+            res.status(404).json({ profile: 'There are no profiles' })
+        );
+});
 
 // @router 	POST api/profile
 // @desc 	Create user profile
