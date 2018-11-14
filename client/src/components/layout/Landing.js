0552c0c0ac6e906805d0b4e0ebae703611f8dd99
andikayopr <ayprihartomo@gmail.com>
2018-11-15 06:42:36 +0700

    Very basic layout setup

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">TalentHub</h1>
+                    <p className="lead">Connecting talented people.</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Landing;
